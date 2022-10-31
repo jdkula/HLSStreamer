6d1b3e06898ef8e01688ec2e246c07110e939627
Jonathan Kula <jonathan@jdkula.dev>
2022-10-31 16:43:12 -0700

    Initial Commit

---
+++ HLSStreamer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HLSStreamer
+//
+//  Created by Jonathan Kula on 10/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HLSStreamer/HLSStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLSStreamerApp.swift
+//  HLSStreamer
+//
+//  Created by Jonathan Kula on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct HLSStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
